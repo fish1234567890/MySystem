62edd5217600c2941f69fbe0a28a6f71c7217f65
fish1234567890 <746411774@qq.com>
2019-03-12 18:24:16 +0800

    qwq

--- src/main/java/com/xyz/MySystem/controller/LoginController.java
+++ src/main/java/com/xyz/MySystem/controller/LoginController.java
@@ -4,14 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.druid.util.StringUtils;
-import com.alibaba.fastjson.JSON;
 import com.xyz.MySystem.bean.LoginReq;
 import com.xyz.MySystem.bean.LoginResp;
 import com.xyz.MySystem.service.UserService;
+
+import redis.clients.jedis.Jedis;
 
 @Controller
 @RequestMapping(value="/user")
@@ -28,12 +28,16 @@ public class LoginController {
 	@RequestMapping(value="/login")
 	public LoginResp login(@RequestBody LoginReq req) {
 		LoginResp resp = new LoginResp();
+		//校验用户传入的用户名和密码格式
 		if(StringUtils.isEmpty(req.getEmail()) && StringUtils.isEmpty(req.getPassword())) {
 			resp.setCode("101");
 			resp.setMsg("必填项为空");
 			return resp;
 		}
+		//校验用户名和密码的正确性
 		boolean flag = service.loginCheck(req.getEmail(),req.getPassword());
+		
+		//设置用户redis信息
 		if(flag == true) {
 			resp.setCode("200");
 			resp.setMsg("成功");
@@ -43,10 +47,9 @@ public class LoginController {
 	}
 	
 	public static void main(String[] args) {
-		LoginReq req = new LoginReq();
-		req.setEmail("74641174@qq.com");
-		req.setPassword("123123");
-		String string = JSON.toJSONString(req);
+		Jedis jedis = new Jedis("106.14.219.152",6379);
+		jedis.set("bill", "100");
+		String string = jedis.get("bill");
 		System.out.println(string);
 	}
 }
